dcac3640914a59fb65369e31383b71d74dd60e52
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2022-06-02 15:53:19 +0800

    Update chk_active.sh

--- chk_active.sh
+++ chk_active.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [pgrep -f "test6.py"] && [pgrep -f "oee_today3.py"] > /dev/null
+if [pgrep -f "test_6.py"] && [pgrep -f "oee_today3.py"] > /dev/null
 then
     echo "All processes of rc-local.service are Running"
 else
