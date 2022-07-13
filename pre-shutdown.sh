2cdca830ed8a50438c46354f83844f81495be140
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2022-07-13 09:54:08 +0800

    Update pre-shutdown.sh

--- pre-shutdown.sh
+++ pre-shutdown.sh
@@ -1,6 +1,26 @@
-#!/bin/sh
-mysqldump -uroot -puta1234 --databases test watch_files mqtt cms mes >> /home/uta_iot/utashare_iotdb/$(date +%Y_%m_%d_%H_%M_%S).sql;
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides: shutdownbefore
+# Required-Start:
+# Required-Stop:
+# Default-Start:    2 3 4 5
+# Default-Stop:     0 1 6
+# Short-Description:
+# Description:
+### END INIT INFO
+case "${1:-''}" in
+  'start')
 
-umount /home/uta_iot/excel_output /home/uta_iot/utashare_iotdb/
+        #开机需要执行的逻辑
+        ;;
+ 'stop')
+        #关机需要执行的逻辑
+		#登出iscsi命令写在这里，本文不展示
+        mysqldump -uroot -puta1234 --databases test watch_files mqtt cms mes >> /home/uta_iot/utashare_iotdb/$(date +%Y_%m_%d_%H_%M_%S).sql;
+        umount /home/uta_iot/excel_output /home/uta_iot/utashare_iotdb/
+        ;;
+  *)
+        ;;
+esac
 
 exit 0
