a0b6600d6cb19ab2af28a15550ac4a2fa1d70757
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2022-07-19 09:47:34 +0800

    Update export_csv.py

--- export_csv.py
+++ export_csv.py
@@ -481,7 +481,7 @@ if __name__ == '__main__':
 				last=oeedf.shape[0]+2
 				min_row=last+1
 				max_row=last+piedf.shape[0]+1
-				for row in range(min_row,max_row):
+				for row in range(min_row+1,max_row+1):
 					ws[f'G{row}'].number_format ='0.0分'
 				data1 = Reference(ws, min_col=7, min_row=last+1, max_col=7, max_row=last+piedf.shape[0]+1)
 				data2 = Reference(ws, min_col=6, min_row=last+1, max_col=6, max_row=last+piedf.shape[0]+1)
