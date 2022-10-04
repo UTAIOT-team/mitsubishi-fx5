05938a043b93f1d921c214b261e372637e0062d1
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2022-10-04 16:35:05 +0800

    Update dbbackup.sh

--- dbbackup.sh
+++ dbbackup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mysqldump -uroot -puta1234 --databases test watch_files mqtt cms mes >> /home/uta_iot/dumpdb/$(date +%Y_%m_%d_%H_%M_%S).sql;
+mv /home/uta_iot/dumpdb/* home/uta_iot/utashare_iotdb/ &
 
 ##/usr/bin/find /home/uta_iot/utashare_iotdb/ -mtime +10  -exec rm -rf {} \;
 
