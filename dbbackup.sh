555976f9ef472dc1a37563473a47d1ab9ec000d8
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2022-10-05 13:28:37 +0800

    Update dbbackup.sh

--- dbbackup.sh
+++ dbbackup.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 START=${SECONDS}
 mysqldump -uroot -puta1234 --databases test watch_files mqtt cms mes >> /home/uta_iot/dumpdb/$(date +%Y_%m_%d_%H_%M_%S).sql;
-END=${SECONDS}
-echo "dumpdb finished in " $END - $START " second(s)"
+END=$(($SECONDS - $START))
+echo "dumpdb finished in" $END "second(s)"
 START=${SECONDS}
-mv /home/uta_iot/dumpdb/* /home/uta_iot/utashare_iotdb/ &
-END=${SECONDS}
-echo "dumpdb finished in " $END - $START " second(s)"
+mv /home/uta_iot/dumpdb/* /home/uta_iot/utashare_iotdb/
+END=$(($SECONDS - $START))
+echo "move files finished in" $END "second(s)"
 ##/usr/bin/find /home/uta_iot/utashare_iotdb/ -mtime +10  -exec rm -rf {} \;
 
 exit 0
