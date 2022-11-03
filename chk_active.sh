30f856145f2b79fa3650a4fff1ed35a690dac408
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2022-11-03 08:43:38 +0800

    Update chk_active.sh

--- chk_active.sh
+++ chk_active.sh
@@ -5,8 +5,18 @@ b=$(pgrep -f "oee_today3.py")
 if [ -n "$a" ] && [ -n "$b" ]
 then
     echo "All processes of rc-local.service are Running"
+    logger "All processes of rc-local.service are Running"
 else
-    echo "not active"
+    if ![ -n "$a" ]
+    then
+        echo "test_6.py not active"
+        logger "test_6.py not active"
+    fi
+    if ![ -n "$b" ]
+    then
+        echo "oee_today3.py not active"
+        logger "oee_today3.py not active"
+    fi
     systemctl stop rc-local.service
     systemctl start rc-local.service
 fi
