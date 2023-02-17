5a5bb95adde8e83fcbbdb218c7fbb6d3e2b37248
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2023-02-17 11:48:14 +0800

    Update test_6.py

--- test_6.py
+++ test_6.py
@@ -245,7 +245,7 @@ if __name__ == '__main__':
 						right.value=10
 						right=right.loc[right.name.isin(left.name)]
 						msgdf2=left.loc[left.name.isin(right.name)]
-						msg2=msg2.loc[:,['name','value']].to_string()
+						msg2=msgdf2.loc[:,['name','value']].to_string()
 						print("msg2",msg2)
 					msg="\n應開機未開機:\n"+msg1+"\n無計畫開機:\n"+msg2
 					if msg:
