076b602e6278387dfb525ad05d586ae9d8744260
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2024-07-12 11:50:01 +0800

    Update chk_active_server.sh

--- chk_active_server.sh
+++ chk_active_server.sh
@@ -6,17 +6,21 @@ service_status=$(systemctl is-active mariadb.service)
 # 如果服务未启动，则启动服务
 if [ "$service_status" != "active" ]; then
     echo "MariaDB 服务未启动，正在启动..."
+    logger "MariaDB 服务未启动，正在启动..."
     systemctl start mariadb.service
 
     # 检查服务是否成功启动
     service_status=$(systemctl is-active mariadb.service)
     if [ "$service_status" == "active" ]; then
         echo "MariaDB 服务已成功启动。"
+        logger "无法启动 MariaDB 服务，请检查日志。"
     else
         echo "无法启动 MariaDB 服务，请检查日志。"
+        logger "无法启动 MariaDB 服务，请检查日志。"
     fi
 else
     echo "MariaDB 服务正在运行。"
+    logger "无法启动 MariaDB 服务，请检查日志。"
 fi
 
 b=$(pgrep -f "oee_today3.py")
