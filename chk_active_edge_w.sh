7a6c318fbc60d08a3b86fb02f354db3b0b5c415c
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2024-08-02 10:34:04 +0800

    Update chk_active_edge_w.sh

--- chk_active_edge_w.sh
+++ chk_active_edge_w.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-a=$(pgrep -f "test_6.py")
+a=$(pgrep -f "test_6_w.py")
 
 if !([ -n "$a" ])
 then
-    echo "test_6.py isn't active, restart rc-local.service."
-    logger "test_6.py isn't active, restart rc-local.service."
+    echo "test_6_w.py isn't active, restart rc-local.service."
+    logger "test_6_w.py isn't active, restart rc-local.service."
     systemctl stop rc-local.service
     systemctl start rc-local.service
 else
-    echo "test_6.py is actived."
-    logger "test_6.py is  actived."
+    echo "test_6_w.py is actived."
+    logger "test_6_w.py is  actived."
 fi
